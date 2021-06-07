32bc886ba5d9d7975f2847fa0a65f52d3a772069
Stephen Aranda <aranda.stephen88@gmail.com>
2021-06-06 21:28:31 -0500

    Implemented server in server.js

--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express')
+const postsRouter = require('./posts/posts-router')
+const server = express()
+
+server.use(express.json())
+server.use('/api/posts',postsRouter)
+
+server.get('/',(req,res)=>{
+    res.send(`
+    <h2> Posts routing</h2>
+    <p> Welcome to my api</p>
+    `  
+    )
+})
+
+module.exports = server;
